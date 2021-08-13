00e293ab51e97e46e741ff22ebb4259056aad67e
Jose de Jesus Miranda Espino <jesus.voluntadheredada.human@gmail.com>
2021-08-12 22:29:55 -0500

    str_replace

--- index1.php
+++ index1.php
@@ -1,13 +1,52 @@
 <?php
 
-$texto = '$560,000';
-$re = str_replace( '$', '', $texto );
-$ca = str_replace( ',', '', $re );
-echo $ca;
+$cadena1 = 'Esta es una cadena de texto';
+$cadena2 = '';
+$cadena3 = 'es';
+
+for( $i=0;$i < strlen( $cadena1 ); $i++  ){
+    $cadena2 = $cadena1[$i] . $cadena2;
+
+}
 
 
+echo '<p>' . $cadena1 . '</p>' ;
+echo '<p>' . $cadena2 . '</p>';
+
+$r = strstr( $cadena1, $cadena3 );
+$p = strpos($cadena1, $cadena3 );
+
+if( $r ){
+    echo '<p>Valor encontrado</p>';
+}else{
+    echo '<p>Valor no encontrado</p>';
+}
 
 
+echo '<p>' . $p . '</p>';
+
+
+$mail = 'mutuelo@gmail.com';
+$arroba = strpos( $mail, '@' );
+$arroba++;
+$punto = strpos( $mail, '.', $arroba );
+$longotud = $punto - $arroba;
+$servicio = substr( $mail, $arroba, $longotud );
+
+echo '<p>' . $servicio . '</p>';
+
+
+echo  '<p> ' .  strtoupper($cadena1) .  '  </p>' ;
+echo  '<p> ' .  strtolower($cadena1) .  '  </p>';
+
+
+echo   is_numeric(5) .  '<br/>';
+echo   is_numeric('5') .  '<br/>';
+echo   is_numeric('cinco') ;
+echo 'hola <br/>';
+echo is_nan( acos( 8 ) )?'No es un numeto':'Es un numero' ;
+echo '-*******- <br/>';
+echo is_finite(log(0))?'Es finito':'No es un numero';
 
 ?>
 
