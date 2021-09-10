27af0ea31e014e9bf4eead5de3c802462b781c72
Jose de Jesus Miranda Espino <jesus.voluntadheredada.human@gmail.com>
2021-09-09 19:57:50 -0500

    flock

--- index.php
+++ index.php
@@ -30,19 +30,45 @@ $archivo1 = 'archivo.txt';
 
 if ( touch($archivo1)){
 
-    $linea1 = 'Este es un texto';
-    $linea2 = 'Este es otro texto';
-    $linea3 = 'Este es un nuevo texto';
-    $linea4 = 'Este es una variante de texto';
+    $bandera = false;
+    $maximo = 100000;
+    $contar = 0;
 
-    $id2 = fopen( $archivo1, 'w'  );
+    while( !$bandera ){
+        if( !is_writable($archivo1) ){
+            $bandera = true;
+            break;
+        }
+        $contar++;
 
-    fwrite($id2, $linea1);
-    fwrite($id2, $linea2);
-    fwrite($id2, $linea3);
-    fwrite($id2, $linea4);
+        if( $contar > $maximo ){
+            break;
+        }
+
+
+        if($bandera){
+            $linea1 = 'Este es un texto';
+            $linea2 = 'Este es otro texto';
+            $linea3 = 'Este es un nuevo texto';
+            $linea4 = 'Este es una variante de texto';
+
+            $id2 = fopen($archivo1, 'w');
+
+            fwrite($id2, $linea1);
+            fwrite($id2, $linea2);
+            fwrite($id2, $linea3);
+            fwrite($id2, $linea4);
+
+            flock( $archivo1, 3 );
+        }
+
+
+    }
+
+
 
     fclose( $id2 );
+    flock($archivo1, 3);
 
 }
 
